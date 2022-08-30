83764d04a780967171fddc64cc06dd768918fc67
713tase <112501038+713tase@users.noreply.github.com>
2022-08-30 18:48:58 +0300

    Update BAC2022 sim.cpp

--- BAC2022 sim.cpp
+++ BAC2022 sim.cpp
@@ -1,1 +1,19 @@
+S II.3
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    int temperatura;
+    cin >> temperatura;
+    if (temperatura > 11)
+        cout << "CALDUROS";
+        else
+        if(temperatura < 10)
+        cout << "RACOROS";
+    else
+        cout << "NORMAL"; // temperatura >= 10 && temperatura <= 11
+
+    return 0;
+}
