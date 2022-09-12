10f0e57610f0123478c30c26382dd9ea8cf83a50
rotti123 <92742818+rotti123@users.noreply.github.com>
2022-09-12 17:16:52 +0300

    Update BAC 2022 sim.cpp

--- BAC 2022 sim.cpp
+++ BAC 2022 sim.cpp
@@ -18,3 +18,25 @@ int main() {
     cout<<0;
   }
 }
+
+III.2
+  #include <iostream>
+using namespace std;
+
+int main() {
+  int flag = 0, n, a[300], x;
+  cin >> n;
+  for (int i = 0; i <= n * 3 - 1; i++) {
+    cin >> a[i];
+  }
+  cin >> x;
+  for (int i = 0; i <= 3 * n - 1; i = i + 3) {
+    if (a[i] == x) {
+      cout << a[i + 1] << " ";
+      flag = 1;
+    }
+  }
+  if (flag == 0) {
+    cout << "NU";
+  }
+}
